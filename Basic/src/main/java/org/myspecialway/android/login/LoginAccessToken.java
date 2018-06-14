48b9733b8d649c9010d01da2ab5c333da3066c48
TalBuch <37792057+TalBuch@users.noreply.github.com>
2018-06-14 17:34:20 +0300

    Login Request (#39)
    * Add retrofit dependencies to build.gradle
    
    * Add LoginClient, IUserLogin,LoginAccessToken and LoginCostants classes to allow login request using retrofit
    
    * Add LoginClientTest

---
+++ Basic/src/main/java/org/myspecialway/android/login/LoginAccessToken.java
@@ -0,0 +1,19 @@
+package org.myspecialway.android.login;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginAccessToken {
+
+        private String accessToken;
+
+        public String getAccessToken() {
+            return accessToken;
+        }
+
+        public void setAccessToken(String accessToken) {
+            this.accessToken = accessToken;
+        }
+
+
+}
---
+++ Basic/src/main/java/org/myspecialway/android/login/LoginConstants.java
@@ -0,0 +1,5 @@
+package org.myspecialway.android.login;
+
+public class LoginConstants {
+    public static final String MSW_SERVER_URL = "https://msw-server.azurewebsites.net";
+}
