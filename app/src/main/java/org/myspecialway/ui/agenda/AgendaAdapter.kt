1fa746d0d6fb70b6a97bdcb0c86e5eb46a02dfc3
iayalon <iayalon@interwize.com>
2018-08-26 18:12:04 +0300

    added icon

--- app/src/main/java/org/myspecialway/ui/agenda/AgendaAdapter.kt
+++ app/src/main/java/org/myspecialway/ui/agenda/AgendaAdapter.kt
@@ -31,6 +31,7 @@ class AgendaAdapter(val onClick: (ScheduleRenderModel) -> Unit) : RecyclerView.A
         private fun drawBorderIfNeeded(schedule: ScheduleRenderModel) {
             if (schedule.isNow) {
                 itemView.card_view.setBackgroundResource(R.drawable.border)
+
             } else {
                 itemView.card_view.setBackgroundResource(android.R.color.white)
             }
