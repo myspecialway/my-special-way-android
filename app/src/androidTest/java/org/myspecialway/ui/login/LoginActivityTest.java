edba5e66d0ec6828c1f1c5b9014b7b3501cf8430
iayalon <iayalon@interwize.com>
2018-09-09 11:20:24 +0300

    checkUserNameFocus added

--- app/src/androidTest/java/org/myspecialway/ui/login/LoginActivityTest.java
+++ app/src/androidTest/java/org/myspecialway/ui/login/LoginActivityTest.java
@@ -1,7 +1,7 @@
 package org.myspecialway.ui.login;
 
 
-import android.os.Handler;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,13 +9,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.myspecialway.R;
-import org.myspecialway.common.Run;
-
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Observable;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
@@ -23,8 +16,8 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.isFocusable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
@@ -32,22 +25,21 @@ import static org.hamcrest.Matchers.not;
 public class LoginActivityTest {
 
     @Rule
-    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
+    public ActivityTestRule<LoginActivity> activityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
     @Test
     public void passwordIsEmpty() {
-        onView(withId(R.id.usernameTextFiled)).perform(typeText("student"), closeSoftKeyboard());
+        onView(withId(R.id.usernameTextFiled)).perform(typeText("stu"), closeSoftKeyboard());
         onView(withId(R.id.passwordTextFiled)).perform(clearText());
         onView(withId(R.id.loginButton)).check(matches(not(isEnabled())));
     }
 
     @Test
     public void userIsEmpty() {
-        onView(withId(R.id.passwordTextFiled)).perform(typeText("student"), closeSoftKeyboard());
+        onView(withId(R.id.passwordTextFiled)).perform(typeText("Aa12"), closeSoftKeyboard());
         onView(withId(R.id.usernameTextFiled)).perform(clearText());
         onView(withId(R.id.loginButton)).check(matches(not(isEnabled())));
     }
-
 
     @Test
     public void loginFailed() {
@@ -56,4 +48,8 @@ public class LoginActivityTest {
         onView(withId(R.id.loginButton)).perform(click());
     }
 
+    @Test
+    public void checkUserNameFocus() {
+        onView(withId(R.id.usernameTextFiled)).check(matches(ViewMatchers.isFocusable()));
+    }
 }
