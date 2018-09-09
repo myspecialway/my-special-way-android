08b9aa357efc8ee7dbdbd0005ab5ff9d91fefe34
iayalon <iayalon@interwize.com>
2018-09-09 11:42:17 +0300

    added progressBarVisibleAfterLogin, checkErrorDialogDisplayedOnBadCredentials, checkUserNameFocus, loginPassed

--- app/src/androidTest/java/org/myspecialway/ui/login/LoginActivityTest.java
+++ app/src/androidTest/java/org/myspecialway/ui/login/LoginActivityTest.java
@@ -1,10 +1,12 @@
 package org.myspecialway.ui.login;
 
 
+import android.preference.PreferenceManager;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.jetbrains.annotations.TestOnly;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +18,18 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
-import static android.support.test.espresso.matcher.ViewMatchers.isFocusable;
+
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
+/**
+ * ======> When running these test the user should be sign out. <==========
+ */
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
 
@@ -42,14 +51,64 @@ public class LoginActivityTest {
     }
 
     @Test
-    public void loginFailed() {
+    public void loginFailed() throws InterruptedException {
         onView(withId(R.id.usernameTextFiled)).perform(typeText("bad user"), closeSoftKeyboard());
         onView(withId(R.id.passwordTextFiled)).perform(typeText("11111"), closeSoftKeyboard());
+
+        // wait for the alpha animation
+        Thread.sleep(500);
         onView(withId(R.id.loginButton)).perform(click());
+
+        //Wait for a constant time of 2 seconds to get the response from server for login
+        Thread.sleep(2000);
+
+        assertFalse(activityTestRule.getActivity().isFinishing());
+    }
+
+    @Test
+    public void progressBarVisibleAfterLoginClick() throws InterruptedException {
+        onView(withId(R.id.usernameTextFiled)).perform(typeText("1"), closeSoftKeyboard());
+        onView(withId(R.id.passwordTextFiled)).perform(typeText("1"), closeSoftKeyboard());
+
+        Thread.sleep(500);
+        onView(withId(R.id.loginButton)).perform(click());
+
+        onView(withId(R.id.progress)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkErrorDialogDisplayedOnBadCredentials() throws InterruptedException {
+        onView(withId(R.id.usernameTextFiled)).perform(typeText("1"), closeSoftKeyboard());
+        onView(withId(R.id.passwordTextFiled)).perform(typeText("1"), closeSoftKeyboard());
+
+        Thread.sleep(500);
+        onView(withId(R.id.loginButton)).perform(click());
+
+        Thread.sleep(4000);
+
+        onView(withId(R.id.closeButton)).check(matches(isDisplayed()));
     }
 
     @Test
     public void checkUserNameFocus() {
         onView(withId(R.id.usernameTextFiled)).check(matches(ViewMatchers.isFocusable()));
     }
+
+    @Test
+    public void loginPassed() throws InterruptedException {
+        onView(withId(R.id.usernameTextFiled)).perform(typeText("student"), closeSoftKeyboard());
+        onView(withId(R.id.passwordTextFiled)).perform(typeText("Aa123456"), closeSoftKeyboard());
+
+        Thread.sleep(500);
+        onView(withId(R.id.loginButton)).perform(click());
+
+        //Wait for a constant time of 2 seconds to get the response from server for login
+        Thread.sleep(4000);
+
+        assertTrue(activityTestRule.getActivity().isFinishing());
+
+        PreferenceManager.getDefaultSharedPreferences(activityTestRule.getActivity()).edit().clear().apply();
+    }
+
+
 }
