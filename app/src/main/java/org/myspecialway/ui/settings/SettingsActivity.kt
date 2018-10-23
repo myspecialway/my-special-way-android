13460340f78388b7db0dbd794553018aeb11e19d
Dardikman, Itay (id1344) <id1344@intl.att.com>
2018-10-23 12:26:41 +0300

    SettingsActivity GUI fixes.

--- app/src/main/java/org/myspecialway/ui/settings/SettingsActivity.kt
+++ app/src/main/java/org/myspecialway/ui/settings/SettingsActivity.kt
@@ -2,20 +2,12 @@ package org.myspecialway.ui.settings
 
 import android.content.Context
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.preference.PreferenceFragment
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import org.myspecialway.App
-import org.myspecialway.R
 import org.myspecialway.common.logout
 import org.myspecialway.ui.login.showLogoutDialog
-import android.widget.LinearLayout
-
-
+import org.myspecialway.R
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -24,58 +16,22 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        if (fragmentManager.findFragmentById(android.R.id.content) == null) {
-            fragmentManager.beginTransaction()
-                    .add(android.R.id.content, SettingsFragment()).commit()
-        }
-
-        //when logout is pressed
-        if(intent?.action.equals("settings.logout")){
-            showDialog()
-        }
+        setContentView(R.layout.activity_settings_layout)
     }
 
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-
-        val root = findViewById<View>(android.R.id.list).parent.parent as LinearLayout
-        val toolbar = LayoutInflater.from(this).inflate(R.layout.toolbar, root, false) as Toolbar
-        root.addView(toolbar, 0) // insert at top
-
-        setSupportActionBar(toolbar)
-        val supportActionBar = supportActionBar
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar.setDisplayShowHomeEnabled(true)
-        supportActionBar.setHomeAsUpIndicator(R.drawable.back)
-    }
-
-
-    private fun showDialog() {
+    fun onLogoutClick(view: View) {
         showLogoutDialog {
-            approveButtonClickListener{
+            approveButtonClickListener {
                 App.instance?.applicationContext?.logout()
             }
 
-            cancelButtonClickListener{
+            cancelButtonClickListener {
                 dialog?.dismiss()
             }
         }
     }
 
-
-    class SettingsFragment : PreferenceFragment() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            addPreferencesFromResource(R.xml.preferences)
-
-        }
-
-        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-            val view = super.onCreateView(inflater, container, savedInstanceState)
-            view.layoutDirection = View.LAYOUT_DIRECTION_RTL
-            return view
-        }
-
+    fun onBackClick(view: View) {
+        finish()
     }
 }
