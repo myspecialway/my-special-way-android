4770fda79e609bf07dcb80ff2e8540ada413648c
noanika <noanika@gmail.com>
2019-01-20 16:59:13 +0200

    Add files via upload
    reset alarm reciever

---
+++ FCMMessagingService.kt
@@ -0,0 +1,104 @@
+package org.myspecialway.fcm
+
+import android.content.Intent
+import android.preference.PreferenceManager
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.JsonObject
+import io.reactivex.schedulers.Schedulers
+import org.koin.android.ext.android.inject
+import org.myspecialway.data.local.LocalDataSource
+import org.myspecialway.data.remote.RemoteDataSource
+import org.myspecialway.ui.agenda.blockedSectionsQuery
+import org.myspecialway.ui.agenda.locationQuery
+import org.myspecialway.ui.agenda.query
+import org.myspecialway.ui.alarms.AlarmsReceiver
+import org.myspecialway.ui.login.UserModel
+
+
+class FCMMessagingService : FirebaseMessagingService() {
+    private val TAG = this@FCMMessagingService.javaClass.simpleName
+
+    private val remote by inject<RemoteDataSource>()
+    private val local by inject<LocalDataSource>()
+
+    override fun onNewToken(token: String?) {
+        Log.d(TAG, "Refreshed token: " + token!!)
+
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val editor = sharedPref.edit()
+        editor.putString("token", token)
+        editor.apply()
+    }
+
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage!!.from!!)
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.data != null) {
+            updateLocations()
+            updateSchedule()
+            updateBlockedSections()
+            resetAlarm()
+        }
+    }
+
+    private fun resetAlarm() {
+        val intent = Intent(AlarmsReceiver.INTERNAL_ALARM_ACTION, null, applicationContext, AlarmsReceiver::class.java)
+        sendBroadcast(intent)
+    }
+
+    private fun updateSchedule() {
+        remote.fetchSchedule(getSchedulePayLoad())
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                        {
+                            local.deleteAllSchedule()
+                            local.saveAllSchedule(it)
+                        },
+                        { })
+    }
+
+    private fun updateLocations() {
+        remote.fetchLocations(getLocationsPayLoad())
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                        {
+                            local.deleteAllLocations()
+                            local.saveLocations(it)
+                        },
+                        { })
+    }
+
+    private fun updateBlockedSections() {
+        remote.fetchBlockedSections(getBlockedSectionsPayLoad())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ local.saveBlockedSections(it) }, { })
+    }
+
+    private fun getLocationsPayLoad(): JsonObject {
+        val json = JsonObject()
+        json.addProperty("query", locationQuery())
+        json.addProperty("value", "")
+        return json
+    }
+
+    private fun getSchedulePayLoad(): JsonObject {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val json = JsonObject()
+        json.addProperty("query", query(UserModel().getUser(sharedPref).id ?: ""))
+        json.addProperty("value", "")
+        return json
+    }
+
+    private fun getBlockedSectionsPayLoad(): JsonObject {
+        val json = JsonObject()
+        json.addProperty("query", blockedSectionsQuery())
+        json.addProperty("value", "")
+        return json
+    }
+}
